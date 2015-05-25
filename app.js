3ee242fac528939d959db4a436f15e5aaf28af6c
dan billingham <dbillinghamuk@yahoo.co.uk>
2015-05-25 19:47:08 +0100

    slack web hook

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
 	bodyParser = require('body-parser'),
-  	mongoose = require('mongoose');
+	mongoose = require('mongoose'),
+	Slack = require('slack-node');
   
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
   
@@ -10,8 +11,21 @@ var config = require('./config.js')[env];
 
 app.post('/test', function(req, res){
   
-   //var searchString = req.body.text;
-	res.send("working");
+	var slackRes = new Slack();
+	slackRes.setWebhook("https://hooks.slack.com/services/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");
+	
+	slackRes.webhook({
+	  channel: "#general",
+	  username: "webhookbot",
+	  text: "This is posted to #general and comes from a bot named webhookbot."
+	}, function(err, response) {
+	  console.log(response);
+	});
+	
+  
+  /*
+   var channelId = req.body.channel_id;
+	res.send("working");*/
 });
 
 app.listen(config.port, function(){
