c9c3ceb9a0e09de54206b299809ec28ca4a9340c
dbillingham <dbillinghamuk@yahoo.co.uk>
2015-06-10 12:05:02 +0100

    Added gulp-clean to remove old es5 babel compiled files

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,28 @@
 var gulp = require("gulp"),
 	sourcemaps = require('gulp-sourcemaps'),
 	babel = require("gulp-babel"),
-	path = require('path');
+	path = require('path'),
+    clean = require("gulp-clean");
 
 var paths = {
     es6: ['es6/**/*.js'],
     es5: 'es5',
     // Must be absolute or relative to source map
-    sourceRoot: path.join(__dirname, 'es6'),
+    sourceRoot: path.join(__dirname, 'es6')
 };
 
-gulp.task('babel', function () {
+gulp.task('babel', ['cleanEs5Dir'], function () {
     return gulp.src(paths.es6)
         .pipe(sourcemaps.init())
         .pipe(babel())
         .pipe(sourcemaps.write('.',
                   { sourceRoot: paths.sourceRoot }))
         .pipe(gulp.dest(paths.es5));
+});
+
+gulp.task('cleanEs5Dir', function () {
+  return gulp.src(paths.es5, {read: false})
+    .pipe(clean());
 });
 
 gulp.task('watch', function() {
